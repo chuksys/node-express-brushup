406625bc57305ee238d1bc4feb64d947d99c70d7
chuksys <chuksagbakuru@gmail.com>
2021-04-21 13:44:10 +0100

    added post route to landing page to send lead email

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,8 @@
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Node-Demo' });
 }
+
+exports.submit_lead = function(req, res, next) {
+    console.log("Lead Email:", req.body.lead_email)
+    res.redirect('/');
+}
