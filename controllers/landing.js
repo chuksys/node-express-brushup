fa2937856358ad6e0095591ac51c57a508753592
chuksys <chuksagbakuru@gmail.com>
2021-04-21 16:31:57 +0100

    Added functionality to post lead_email to mysql dbTable for Leads

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,9 +1,14 @@
 
+const models = require('../db/models');
+
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Node-Demo' });
 }
 
 exports.submit_lead = function(req, res, next) {
-    console.log("Lead Email:", req.body.lead_email)
-    res.redirect('/');
+    return models.Lead.create({
+        email: req.body.lead_email,
+    }).then(lead => {
+        res.redirect('/');
+    })
 }
