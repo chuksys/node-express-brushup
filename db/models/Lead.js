52aad9ee764f252655e8ab6ccfe7b18ec172b8ef
chuksys <chuksagbakuru@gmail.com>
2021-04-21 16:14:01 +0100

    Created Leads migration file, Lead model and initialized sequelize in bin/www

---
+++ db/models/Lead.js
@@ -0,0 +1,15 @@
+'use strict'
+module.exports = (Sequelize, DataTypes) => {
+    var Lead = Sequelize.define('Lead', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+};
