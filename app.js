25d81d155ec0edd20663a8794cb6d1bc5d3fdc1e
chuksys <chuksagbakuru@gmail.com>
2021-04-22 20:10:04 +0100

    Added connect-flash package to app.js

--- app.js
+++ app.js
@@ -5,17 +5,19 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('passport')
 const session = require('express-session');
+const flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 require('./passport_setup')(passport)
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
