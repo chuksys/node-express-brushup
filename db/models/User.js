a58292db5a2f720cc3227bc92032278d9b448fb6
chuksys <chuksagbakuru@gmail.com>
2021-04-22 12:05:39 +0100

    Added User Model and User Migration files

---
+++ db/models/User.js
@@ -0,0 +1,35 @@
+'use strict'
+module.exports = (Sequelize, DataTypes) => {
+    var User = Sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+    });
+
+    return User;
+};
