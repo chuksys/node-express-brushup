643435a0c31d4ccd329a17a85507f4a92bee5446
chuksys <chuksagbakuru@gmail.com>
2021-04-23 20:14:29 +0100

    Completed UpdateLead

--- routes/api/leads.js
+++ routes/api/leads.js
@@ -14,4 +14,26 @@ router.post('/', (req, res) => {
     }).then(lead => res.status(200).send(lead)); 
 })
 
+router.put('/:id', (req, res) => {
+    return models.Lead.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(lead => {
+        if(lead != null) {
+            return models.Lead.update(
+                {
+                   email: req.body.email
+                }, {
+                    where: {
+                        id: lead.id,
+                    }
+                })
+            .then(lead => res.status(201).send({ Success: true }))
+        } else {
+            res.status(403).send({message: "Resource does not exist"});
+        }
+    })
+})
+
 module.exports = router
